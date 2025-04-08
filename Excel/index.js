340f8028be245d56abf30d0a1444a014ae989c25
Ricardo Aponte <ricardo.aponte@latamready.com>
2025-04-08 15:15:46 -0500

    aaa

--- Excel/index.js
+++ Excel/index.js
@@ -7,9 +7,9 @@ import { fileURLToPath } from "url";
 const PORT = 3008;
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
