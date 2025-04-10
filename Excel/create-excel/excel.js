873233aea8c71f6b7ea083768243cf95a09da70d
Ricardo Aponte <ricardo.aponte@latamready.com>
2025-04-10 11:05:30 -0500

    data optimizada

--- Excel/create-excel/excel.js
+++ Excel/create-excel/excel.js
@@ -1,71 +1,69 @@
 import Exceljs from "exceljs";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 
-const title = (sheet, body)=>{
+const title = (sheet, body) => {
     let title = JSON.parse(body.rptWords);
-    let dataTitle = Object.values(title)
+    let dataTitle = Object.values(title);
     sheet.getCell("B2").value = dataTitle[0];
-    sheet.getCell('B2').style = {
+    sheet.getCell("B2").style = {
         font: {
-          name: 'Arial',
-          size: 10,
-          bold: true
+            name: "Arial",
+            size: 10,
+            bold: true,
         },
-      }
+    };
 
     for (let i = 1; i <= dataTitle.length; i++) {
-        sheet.getCell(`B${3+i}`).value = dataTitle[i];
-        sheet.getCell(`B${3+i}`).style = {
+        sheet.getCell(`B${3 + i}`).value = dataTitle[i];
+        sheet.getCell(`B${3 + i}`).style = {
             font: {
-              name: 'Arial',
-              size: 9,
-              bold: true
+                name: "Arial",
+                size: 9,
+                bold: true,
             },
-          }
+        };
     }
+};
 
-}
-
-const generate = (transaction) =>{
+const generate = (transaction) => {
     let body = JSON.parse(transaction);
-    
+
     for (let i = 0; i < 500000; i++) {
-    // for (let i = 0; i < 50; i++) {
-        body.push(body[0])
+        // for (let i = 0; i < 50; i++) {
+        body.push(body[0]);
     }
     return body;
-}
+};
 
-const insertRows = async(body, sheet)=>{
-
-    sheet.columns = sheet.columns.map(column => {
+const insertRows = async (body, sheet) => {
+    sheet.columns = sheet.columns.map((column) => {
         return {
             ...column,
-            style: { 
+            style: {
                 border: {
-                  top: { style: 'thin' },
-                  left: { style: 'thin' },
-                  bottom: { style: 'thin' },
-                  right: { style: 'thin' }
-                }
-              }
+                    top: { style: "thin" },
+                    left: { style: "thin" },
+                    bottom: { style: "thin" },
+                    right: { style: "thin" },
+                },
+            },
         };
     });
 
     let size = 50000;
     let progress = 0;
     const TOTAL_ROWS = body.length;
-    console.log("total", TOTAL_ROWS) 
+    console.log("total", TOTAL_ROWS);
     while (progress < TOTAL_ROWS) {
         let batch = body.slice(progress, progress + size);
-        batch.forEach(element=>{
-            let arrayTmp = element.map(e => e.value);
-            
+        batch.forEach((element) => {
+            let arrayTmp = element.map((e) => e.value);
+
             sheet.addRow(arrayTmp).commit();
 
             progress++;
@@ -73,14 +71,16 @@ const insertRows = async(body, sheet)=>{
 
         if (progress % 100000 === 0 || progress === TOTAL_ROWS) {
             console.log(`Procesadas ${progress}/${TOTAL_ROWS} filas`);
-            await new Promise(resolve => setImmediate(resolve));
+            await new Promise((resolve) => setImmediate(resolve));
         }
     }
-}
+};
 
-const generateExcel = async(port, count)=>{
+const generateExcel = async (count) => {
     console.log("Iniciando la generación del Excel ......");
-    let testinHeader = JSON.parse(fs.readFileSync(path.join(__dirname, `../tmp/file1.json`)))
+    let testinHeader = JSON.parse(
+        await fs.readFile(path.join(__dirname, `../tmp/file1.json`))
+    );
     let date = new Date();
     const fileName = `Reporte${date.getMilliseconds()}.xlsx`;
     const filePath = path.join(__dirname, "../reportes", fileName);
@@ -88,48 +88,51 @@ const generateExcel = async(port, count)=>{
     const workbook = new Exceljs.stream.xlsx.WorkbookWriter({
         stream: fileStram,
         useSharedStrings: false,
-        useStyles: true
-    })
-    
+        useStyles: true,
+    });
+
     const sheet = workbook.addWorksheet("Mi Excel");
-    title(sheet, testinHeader)
-    let header = JSON.parse(testinHeader.headers)
+    title(sheet, testinHeader);
+    let header = JSON.parse(testinHeader.headers);
     let rowHeaders = sheet.addRow(header);
-    let headerStyle = { 
+    let headerStyle = {
         border: {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+            top: { style: "thin" },
+            left: { style: "thin" },
+            bottom: { style: "thin" },
+            right: { style: "thin" },
         },
         fill: {
-            type: 'pattern',
-            pattern: 'solid',
-            fgColor: { argb: 'FFC8E6C9' } 
+            type: "pattern",
+            pattern: "solid",
+            fgColor: { argb: "FFC8E6C9" },
         },
-    }
-    rowHeaders.eachCell(cell =>{
+    };
+    rowHeaders.eachCell((cell) => {
         cell.border = headerStyle.border;
-        cell.fill = headerStyle.fill
-    })
+        cell.fill = headerStyle.fill;
+    });
 
     rowHeaders.commit();
     let dataBody = [];
     for (let i = 1; i <= count; i++) {
-        let data = fs.readFileSync(path.join(__dirname, `../tmp/file${i}.json`), "utf8")
-        dataBody = dataBody.concat(JSON.parse(JSON.parse(data).transactions))
-        console.log("numero de data que se esta obteniendo " + i)
+        let data = await fs.readFile(
+            path.join(__dirname, `../tmp/file${i}.json`),
+            "utf8"
+        );
+        dataBody = dataBody.concat(JSON.parse(JSON.parse(data).transactions));
+        console.log("numero de data que se esta obteniendo " + i);
     }
-    await insertRows(dataBody, sheet)
-    
+    await insertRows(dataBody, sheet);
+
     await workbook.commit();
     fileStram.end();
-    console.log("Terminando la creación del excel ........")
+    console.log("Terminando la creación del excel ........");
     return {
-        "fileName": fileName,
-        "filePath": filePath,
-        "fileDonwload": `http://149.130.162.8:3008/api/download/${fileName}`
-    }
-}
+        fileName: fileName,
+        filePath: filePath,
+        fileDonwload: `http://149.130.162.8:3008/api/download/${fileName}`,
+    };
+};
 
-export default {generateExcel}
+export default { generateExcel };
--- Excel/index.js
+++ Excel/index.js
@@ -3,6 +3,7 @@ import Excel from "./create-excel/excel.js"
 import fs, { copyFileSync } from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from 'console';
 
 const PORT = 3008;
 
@@ -19,30 +20,30 @@ let dataArray = [];
 app.use(express.json());
 
 
-const eliminatePath = (path, time)=>{
-    setTimeout(()=>{
+const eliminatePath = (path, time) => {
+    setTimeout(() => {
         fs.unlinkSync(`./reportes/${path}`);
     }, time)
-    
+
 }
 
-app.get("/", async(req, res)=>{
+app.get("/", async (req, res) => {
     try {
         console.log("todo correcto")
         res.status(200).send("Holla mi kinggg")
-        
+
     } catch (error) {
         console.log("error", error)
         console.log("error stack", error.stack)
     }
 })
 
-app.get("/api/download/:fileName", async(req, res)=>{
+app.get("/api/download/:fileName", async (req, res) => {
     try {
         let url = path.join(__dirname, "./reportes", req.params.fileName)
         console.log("todo correcto")
         res.status(200).download(url)
-        
+
     } catch (error) {
         console.log("error", error)
         console.log("error stack", error.stack)
@@ -50,55 +51,63 @@ app.get("/api/download/:fileName", async(req, res)=>{
     }
 })
 
-app.post("/api/prueba", async(req, res)=>{
+app.post("/api/prueba", async (req, res) => {
     try {
         let body = req.body;
         if (body.valor == body.cantidad) {
             testingArray.push(body.data);
             res.status(200).send(JSON.stringify(testingArray))
-            testingArray = [];  
-        }else{
+            testingArray = [];
+        } else {
             testingArray.push(body.data);
             res.status(200).send("ingresado correctamente")
         }
-        
+
     } catch (error) {
         res.status(400).send(error.stack)
     }
 })
 
-app.post("/api/create-excel", async (req,res)=>{
+app.post("/api/create-excel", async (req, res) => {
     try {
         console.log("Estoy usando entrando en el body.")
         req.setTimeout(300000)
         let body = req.body;
-        if (body.sumCount === body.count) {
-            // dataArray.push(body);
-            fs.writeFileSync(path.join(__dirname, `./tmp/file${body.sumCount}.json`), JSON.stringify(body))
-            console.log("sumaaaaaa",body.sumCount)
-            // res.status(200).send(dataArray.length)
-            let data = await Excel.generateExcel(PORT, body.count);
-            let downloadUrl = data.fileDonwload
-            let filePath = data.filePath
-            
-            let response = {
-                success: true,
-                message: 'Excel generado correctamente, recuerda que la descarga se eliminara 24 h de su creación',
-                downloadUrl,
-                filePath,
-                size: `${(fs.statSync(data.filePath).size / (1024 * 1024)).toFixed(2)} MB`
-            };
-            console.log("response", response)
-            res.status(200).json(response);
-            
-            eliminatePath(data.fileName, 86400 * 1000);
-            
-        }else{
-            console.log("sumaaaaaa",body.sumCount)
-            fs.writeFileSync(path.join(__dirname, `./tmp/file${body.sumCount}.json`), JSON.stringify(body));
-            res.status(200).send("La data se esta cargando")
-        }
-        // eliminatePath(data.fileName, 50 * 1000);
+        let filePath = path.join(__dirname, `./tmp/file${body.sumCount}.json`);
+        let jsonData = JSON.stringify(filePath);
+        const writeStream = fs.createWriteStream(filePath);
+        writeStream.write(jsonData);
+        writeStream.end();
+
+        writeStream.on("finish", async () => {
+            if (body.sumCount === body.count) {
+                console.log("sumaaaaaa", body.sumCount)
+                let data = await Excel.generateExcel(body.count);
+                let downloadUrl = data.fileDonwload
+                let filePath = data.filePath
+
+                let response = {
+                    success: true,
+                    message: 'Excel generado correctamente, recuerda que la descarga se eliminara 24 h de su creación',
+                    downloadUrl,
+                    filePath,
+                    size: `${(fs.statSync(data.filePath).size / (1024 * 1024)).toFixed(2)} MB`
+                };
+                console.log("response", response)
+                res.status(200).json(response);
+
+                eliminatePath(data.fileName, 86400 * 1000);
+
+            } else {
+                console.log("sumaaaaaa", body.sumCount)
+                res.status(200).send("La data se esta cargando")
+            }
+        })
+
+        writeStream.on("error", (error) => {
+            console.error("Error al guardar archivo: ", err);
+            res.status(500).send("Error al guardar archivo");
+        })
     } catch (error) {
         console.log("error", error)
         console.log("error stack ", error.stack)
@@ -108,4 +117,4 @@ app.post("/api/create-excel", async (req,res)=>{
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor activo en http://149.130.162.8:${PORT}`);
-  });
+});
